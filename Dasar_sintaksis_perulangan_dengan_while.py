bc86e6df7a0a164a9870b8fe395f28a95addc1e0
Athallah Nuzul <athallahnuzul31104@gmail.com>
2022-11-15 05:16:11 +0700

    Konsep Sequensial & Percabangan

--- Dasar_sintaksis_perulangan_dengan_while.py
+++ Dasar_sintaksis_perulangan_dengan_while.py
@@ -2,11 +2,11 @@
 Program Perulangan Membaca Buku dengan While
 """
 print('Perintah Ibu,"Baca semua bukumu!"')
-jumlah_buku=10
+jumlah_buku = 100
 print(f"Jumlah buku {jumlah_buku}")
 jumlah_buku_yang_sudah_dibaca=0
-print(f"Jumlah buku yang dibaca {jumlah_buku_yang_sudah_dibaca}")
 
-for jumlah_buku_yang_sudah_dibaca in range(1,jumlah_buku+1):
+while jumlah_buku_yang_sudah_dibaca < jumlah_buku:
+    jumlah_buku_yang_sudah_dibaca = jumlah_buku_yang_sudah_dibaca + 1
     print(f"Buku ke {jumlah_buku_yang_sudah_dibaca} sudah dibaca")
 print("selesai")
