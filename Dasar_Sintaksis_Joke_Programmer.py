ad322371e71b31c80dbac25b3d829d9f2e1e72a6
Athallah Nuzul <athallahnuzul31104@gmail.com>
2022-11-15 05:05:46 +0700

    Konsep Sequensial & Percabangan

--- Dasar_Sintaksis_Joke_Programmer.py
+++ Dasar_Sintaksis_Joke_Programmer.py
@@ -2,7 +2,7 @@
 Semua sintaksis dasar terdiri dari:
 1. Sequensial : Langkah mengulang
 2. Percabangan : Langkah melompat jika kondisi terpenuhi
-3. Perulangan : Mengulang langkah yang sama berkali kali sampai konddisi terpenuhi
+3. Perulangan : Mengulang langkah yang sama berkali kali sampai kondisi terpenuhi
 """
 # Sequensial
 print('Ibu berkata,"Pergi ke toko"')
---
+++ Dasar_Sintaksis_Percabangan.py
@@ -0,0 +1,16 @@
+"""
+Program Perulangan Membaca Buku
+"""
+print('Perintah Ibu,"Baca semua bukumu!"')
+jumlah_buku=10
+print(f"Jumlah buku {jumlah_buku}")
+jumlah_buku_yang_sudah_dibaca=0
+print(f"Jumlah buku yang dibaca {jumlah_buku_yang_sudah_dibaca}")
+
+for jumlah_buku_yang_sudah_dibaca in range(1,jumlah_buku+1):
+    print(f"Buku ke {jumlah_buku_yang_sudah_dibaca} sudah dibaca")
+print("selesai")
+
+
+
+
