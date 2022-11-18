f75b4e03c7244d790ae2b5c116d379b597f249c1
Athallah Nuzul <athallahnuzul31104@gmail.com>
2022-11-18 07:25:58 +0700

    Belajar Sintaksis di Replit

---
+++ replit_day1.py
@@ -0,0 +1,19 @@
+#Command Print
+print("""Mark
+November 18th 2022
+I am Signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happen to be, no excuses for not coding from the middle of a field!
+I am feeling hype for this.
+""")
+#Command Input
+print("Getting to know you!")
+yourName = input("What is your name ? ")
+print("You are")
+print(yourName)
+your_favorite_food=input("What is your favorite food ? ")
+print("You're probably hungry for")
+print(your_favorite_food)
+your_adress = input("Where do you live ? ")
+print("you probably live happily in")
+print(your_adress)
