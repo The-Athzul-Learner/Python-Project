3c8e97fe7b43de27e4359039f188ff880fba7afb
Athallah <athallah.hartamawangsyah@gmail.com>
2022-11-23 10:14:44 +0700

    Learning in Replit day 1

---
+++ replit_day2.py
@@ -0,0 +1,12 @@
+#Combine string and variables into a simple automatic sentence
+foodName=input("Name a food:")
+type_of_plant=input("Name a type of plant:")
+method=input("Name a method of cooking:")
+wordA=input("A word to describe burned food:")
+item=input("Mention a household item:")
+print()
+print("""Menu""")
+print()
+print(method,foodName,"with",wordA, type_of_plant,"on a bunch of",item)
+
+#
